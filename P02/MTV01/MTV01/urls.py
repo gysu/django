aedb83684492eb4f236ad67cee1b0a490bac6126
gysu <qzero735280@gmail.com>
2021-08-05 11:43:49 +0800

    add template

--- P02/MTV01/MTV01/urls.py
+++ P02/MTV01/MTV01/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from mysite.views import about,listing,listing2
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', about),
+    path('listing/', listing),
+    path('list/', listing2),
+
+
 ]
--- P02/MTV01/mysite/admin.py
+++ P02/MTV01/mysite/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from mysite.models import NewTable,Product
 # Register your models here.
+admin.site.register(NewTable)
+
+class ProductAdmin(admin.ModelAdmin):
+  list_display =("name","price","qty","size")
+
+admin.site.register(Product,ProductAdmin)
+
